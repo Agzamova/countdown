58d50c0c4ed97ba0f2055c595ba8154828502930
Natalia Agzamova <109974405+Agzamova@users.noreply.github.com>
2023-02-02 18:04:01 +0500

    countdown to my birthday

---
+++ script.js
@@ -0,0 +1,31 @@
+let day = document.querySelector('.days')
+let hour = document.querySelector('.hours')
+let minute = document.querySelector('.minutes')
+let second = document.querySelector('.seconds')
+
+document.addEventListener('DOMContentLoaded', function() {
+    let deadline = new Date(2023, 06, 16)
+    let timerId = null
+
+    function countdownTimer() {
+        let diff = deadline - new Date()
+
+        if (diff <= 0) {
+            clearInterval(timerId);
+        }
+        
+        let days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0
+        let hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0
+        let minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0
+        let seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0
+
+        day.innerHTML = `${days} <span>days</span>`
+        hour.innerHTML = `${hours} <span>hours</span>`
+        minute.innerHTML = `${minutes} <span>minutes</span>`
+        second.innerHTML = `${seconds} <span>seconds</span>`
+    }
+
+    countdownTimer()
+
+    timerId = setInterval(countdownTimer, 1000);
+})
